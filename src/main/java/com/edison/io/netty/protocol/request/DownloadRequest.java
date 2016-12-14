3ae6121c5fcc5d4e06634df7e4912584db23ba3f
wangzhengfei <wangzhengfei@51jk.com>
2016-12-14 14:22:11 +0800

    add toString

--- src/main/java/com/edison/io/netty/protocol/request/DownloadRequest.java
+++ src/main/java/com/edison/io/netty/protocol/request/DownloadRequest.java
@@ -1,5 +1,7 @@
 package com.edison.io.netty.protocol.request;
 
+import com.edison.io.netty.util.HexUtils;
+
 /**
  * Created by admin on 2016/12/14.
  */
@@ -15,4 +17,13 @@ public class DownloadRequest extends AbstractRequest{
         this.md5Key = md5Key;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UploadRequest{");
+        sb.append("cmd=").append(HexUtils.toHexString(cmd)).append(',');
+        sb.append("requestId=").append(requestId).append(',');
+        sb.append("md5Key=").append(md5Key).append(',');
+        sb.append('}');
+        return sb.toString();
+    }
 }
